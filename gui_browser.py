137d3f52059c6373864fe990d87045e2e8ca0662
Fabian Lesniak <fabian@lesniak-it.de>
2017-09-15 01:46:46 +0200

    load track to player WIP

--- gui_browser.py
+++ gui_browser.py
@@ -1,5 +1,5 @@
 import logging
-from PyQt5.QtWidgets import QComboBox, QHeaderView, QLabel, QPushButton, QTableView, QHBoxLayout, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QComboBox, QHeaderView, QLabel, QPushButton, QTableView, QTextEdit, QHBoxLayout, QVBoxLayout, QWidget
 from PyQt5.QtGui import QPalette, QStandardItem, QStandardItemModel
 from PyQt5.QtCore import Qt, pyqtSignal
 
@@ -30,6 +30,7 @@ class Browser(QWidget):
     self.slot = None # set after selecting slot in media menu
     self.menu = None
     self.sort = "default"
+    self.track_id = None
     self.setWindowTitle("Browse Player {}".format(self.player_number))
 
     self.request = None # requests are parsed on signaling handleRequestSignal
@@ -58,7 +59,7 @@ class Browser(QWidget):
     top_layout.addWidget(self.back_button)
     top_layout.setStretch(0, 1)
 
-    # lower part
+    # mid part
     self.model = QStandardItemModel(self)
     self.view = QTableView(self)
     self.view.setModel(self.model)
@@ -70,10 +71,36 @@ class Browser(QWidget):
     self.view.setStyleSheet("QTableView { background-color: black; } QTableView::item:focus { background-color: darkslategray; selection-background-color: black; }")
     self.view.clicked.connect(self.tableItemClicked)
 
+    # metadata
+    self.metadata_label = QLabel("Metadata:", self)
+    self.metadata_edit = QTextEdit()
+    self.metadata_edit.setReadOnly(True)
+
+    metadata_layout = QVBoxLayout()
+    metadata_layout.addWidget(self.metadata_label)
+    metadata_layout.addWidget(self.metadata_edit)
+
+    mid_layout = QHBoxLayout()
+    mid_layout.addWidget(self.view)
+    mid_layout.addLayout(metadata_layout)
+
+    # lower part (load buttons)
+    buttons_layout = QHBoxLayout()
+    self.load_buttons = []
+    for i in range(1,5):
+      btn = QPushButton("Load Player {}".format(i))
+      btn.setFlat(True)
+      btn.setEnabled(False)
+      btn.clicked.connect(lambda: self.loadIntoPlayer(i))
+      buttons_layout.addWidget(btn)
+      self.load_buttons += [btn]
+
     layout = QVBoxLayout(self)
     layout.addLayout(top_layout)
-    layout.addWidget(self.view)
+    layout.addLayout(mid_layout)
+    layout.addLayout(buttons_layout)
 
+    self.updateButtons()
     self.mediaMenu()
 
   def mediaMenu(self):
@@ -144,7 +171,16 @@ class Browser(QWidget):
       for column in columns:
         row += [makeItem(str(entry[column]), data)]
       self.model.appendRow(row)
-    #self.view.update()
+
+  def metadata(self, slot, track_id):
+    self.prodj.dbs.get_metadata(self, self.player_number, slot, track_id, self.storeRequest)
+
+  def renderMetadata(self, request, source_player_number, slot, track_id, metadata):
+    md = ""
+    for key in [k for k in ["title", "artist", "album", "genre", "key", "bpm", "comment", "rating", "duration"] if k in metadata]:
+      md += "{}:\t{}\n".format(key.title(), metadata[key])
+    self.metadata_edit.setText(md)
+    self.track_id = track_id
 
   def backButtonClicked(self):
     if self.menu in ["title", "artist", "album"]:
@@ -172,14 +208,26 @@ class Browser(QWidget):
         self.albumMenu(self.slot, "default")
       else:
         logging.warning("Browser: root menu type %s not implemented yet", data["name"])
+    elif data["type"] == "title":
+      self.metadata(self.slot, data["track_id"])
     else:
       logging.warning("Browser: unhandled click type %s", data["type"])
+    self.updateButtons() # update buttons for convenience
 
   def sortChanged(self):
     self.sort = self.sort_box.currentData()
     if self.menu in ["title"]:
       logging.debug("sort changed to %s", self.sort)
       self.titleMenu(self.slot, self.sort)
+
+  def loadIntoPlayer(self, player_number):
+    logging.debug("Browser: loading track (pn %d slot %s tid %d) into player %d",
+      self.player_number, self.slot, self.track_id, player_number)
+    self.prodj.vcdj.command_load_track(player_number, self.player_number, self.slot, self.track_id)
+
+  def updateButtons(self):
+    for i in range(1,5):
+      self.load_buttons[i-1].setEnabled(i in self.prodj.gui.players)
 
   # special request handling to get into qt gui thread
   # storeRequest is called from outside (non-qt gui)
