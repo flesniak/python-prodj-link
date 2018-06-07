82d223cc3d201a805aa119c9b620b6e019c49408
Fabian Lesniak <fabian@lesniak-it.de>
2018-06-07 12:41:09 +0200

    ip: check all addresses of an interface
    This commit drops a lot of stale code left over in ip.py and
    checks every ip address on any interface. Additionally, a
    more pythonic way to check for addresses inside an IPv4 network
    using IPv4Network is used.

--- ip.py
+++ ip.py
@@ -1,52 +1,25 @@
-import fcntl
-import socket
 import netifaces as ni
-from ipaddress import IPv4Network
-import struct
+from ipaddress import IPv4Address, IPv4Network
 import logging
 
-def get_ip_address(addrs):
-  if ni.AF_INET in addrs:
-    return addrs[ni.AF_INET][0]['addr']
-
-  return '127.0.0.1'
-
-def get_netmask(addrs):
-  if ni.AF_INET in addrs:
-    return addrs[ni.AF_INET][0]['netmask']
-
-  return '255.255.255.0'
-
-def get_mac_address(addrs):
-  if ni.AF_LINK in addrs:
-    return addrs[ni.AF_LINK][0]['addr']
-
-  return ''
-
-#https://stackoverflow.com/questions/819355/how-can-i-check-if-an-ip-is-in-a-network-in-python
-def address_is_in_network(ip, net_n_bits):
-  ipaddr = struct.unpack('!L', socket.inet_aton(ip))[0]
-  net, bits = net_n_bits.split('/')
-  netaddr = struct.unpack('!L', socket.inet_aton(net))[0]
-  netmask = (0xFFFFFFFF >> int(bits)) ^ 0xFFFFFFFF
-  return ipaddr & netmask == netaddr
-
-def guess_own_iface(sock, match_ips):
+def guess_own_iface(match_ips):
   if len(match_ips) == 0:
     return None
-  for idx, iface in socket.if_nameindex():
-    try:
-      addr = ni.ifaddresses(iface)
-      ip = get_ip_address(addr)
-      netmask = get_netmask(addr)
-      mac = get_mac_address(addr)
-    except OSError as e:
-      if e.errno == 99:
-        logging.warning("{} has no IPv4 address".format(iface))
-      else:
-        logging.warning("Failed to get interface information for {}: {} ({})".format(iface,e.strerror,e.errno))
+
+  for iface in ni.interfaces():
+    ifa = ni.ifaddresses(iface)
+
+    if ni.AF_LINK not in ifa or len(ifa[ni.AF_LINK]) == 0:
+      logging.debug("{} is has no MAC address, skipped.".format(iface))
       continue
-    net = IPv4Network(ip+"/"+netmask, strict=False)
-    if any([match_ip for match_ip in match_ips if address_is_in_network(match_ip, net.with_prefixlen)]):
-      return iface, ip, netmask, mac
+    if ni.AF_INET not in ifa or len(ifa[ni.AF_INET]) == 0:
+      logging.warning("{} has no IPv4 address".format(iface))
+      continue
+
+    mac = ifa[ni.AF_LINK][0]['addr']
+    for addr in ifa[ni.AF_INET]:
+      net = IPv4Network(addr['addr']+"/"+addr['netmask'], strict=False)
+      if any([IPv4Address(ip) in net for ip in match_ips]):
+        return iface, addr['addr'], addr['netmask'], mac
+
   return None
