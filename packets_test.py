13b3c37a39f03628caac220a542c5559a7866e54
Jonas Liljestrand <jonas.liljestrand@gmail.com>
2019-06-03 22:46:41 +0200

    Add unittest for parsing packets

---
+++ packets_test.py
@@ -0,0 +1,32 @@
+import unittest
+from packets import DBField
+from construct import Container
+
+
+class TestPackets(unittest.TestCase):
+
+    def test_string_parsing(self):
+        field = DBField.parse(
+            b"\x26\x00\x00\x00\x0a\xff\xfa\x00\x48\x00\x49\x00" +
+            b"\x53\x00\x54\x00\x4f\x00\x52\x00\x59\xff\xfb\x00\x00"
+        )
+
+        self.assertEqual(
+            field,
+            Container(type='string')(value="\ufffaHISTORY\ufffb"),
+        )
+
+        field = DBField.parse(
+            b"\x26\x00\x00\x00\x0b\xff\xfa\x00\x50\x00\x4c\x00" +
+            b"\x41\x00\x59\x00\x4c\x00\x49\x00\x53\x00\x54\xff\xfb" +
+            b"\x00\x00"
+        )
+
+        self.assertEqual(
+            field,
+            Container(type='string')(value="\ufffaPLAYLIST\ufffb"),
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
