85e52f789b458f5a65f447dc901826f6a68ef1ad
Fabian Lesniak <fabian.lesniak@kit.edu>
2021-09-25 18:54:30 +0200

    construct v2.10 compatibility: embed Structs previously using Embedded directly

--- prodj/pdblib/pdbfile.py
+++ prodj/pdblib/pdbfile.py
@@ -1,9 +1,25 @@
-from construct import Array, Embedded, GreedyRange, Struct, Tell
-from .fileheader import FileHeader
+from construct import Array, Const, GreedyRange, Int32ul, Padding, Struct, Tell
 from .page import AlignedPage
+from .pagetype import PageTypeEnum
+
+FileHeaderEntry = Struct(
+  "page_type" / PageTypeEnum,
+  "empty_candidate" / Int32ul,
+  "first_page" / Int32ul, # always points to a strange page, which then links to a real data page
+  "last_page" / Int32ul
+)
 
 PDBFile = Struct(
-  Embedded(FileHeader),
+  Padding(4), # always 0
+  "page_size" / Const(4096, Int32ul),
+  "page_entries" / Int32ul, # FileHeaderEntry follow, usually 20
+  "next_unused_page" / Int32ul, # even unreferenced -> not used as any "empty_candidate", points "out of file"
+  "unknown1" / Int32ul, # (5,4,4,1,1,1...)
+  "sequence" / Int32ul, # sequence number, always incremented by 1 (sometimes 2/3)
+  Padding(4), # always 0
+  "entries" / Array(lambda ctx: ctx.page_entries, FileHeaderEntry),
+  "length" / Tell, # usually 348 when page_entries=20
+  Padding(lambda ctx: ctx.page_size-ctx.length),
   "pages" / GreedyRange(AlignedPage),
   "file_size" / Tell
 )
